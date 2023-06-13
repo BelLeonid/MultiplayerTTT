72e929fab13237d7c6d219c5f5f46e79eb024637
BelLeonid <41448224+BelLeonid@users.noreply.github.com>
2023-06-13 19:53:44 +0300

    Cleaning up index.js

--- backend/index.js
+++ backend/index.js
@@ -31,10 +31,6 @@ io.on("connection", socket => {
         console.log("Match started");
     }
 
-    // if (gameState.getGameStarted()) {
-    //     socket.emit("continue", gameState.getActivePlayer(), gameState.board.getBoard());
-    // }
-
     socket.on("turn", (turn) => {
         console.log(`Turn by Player ${playerSlot}: ${turn.x}, ${turn.y}`);
         if (gameState.getGameOver()) return;
